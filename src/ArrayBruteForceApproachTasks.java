6370ba89a3293acefdb6aeb77042b601b9f8d949
shivamstech <shivamsharma.code@gmail.com>
2026-06-10 18:25:28 +0530

    BruteForceApproachTaskupdate10 commit

--- src/ArrayBruteForceApproachTasks.java
+++ src/ArrayBruteForceApproachTasks.java
@@ -1,6 +1,39 @@
 public class ArrayBruteForceApproachTasks {
     public static void main(String[] args) {
 
+        // * WAP to find the intersection of two arrays (Common Elements)
+        // [1,2,3,4,5]+[3,4,5,6,7] = [3,4,5]
+
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {3, 4, 5, 6, 7};
+
+        int n1=arr1.length;
+        int n2=arr2.length;
+
+        int index=0;
+
+
+        int[] newArr = new int[Math.min(arr1.length, arr2.length)];
+
+        for(int i=0; i<n1; i++)
+        {
+            for(int j=0; j<n2; j++)
+            {
+                if(arr1[i]==arr2[j])
+                {
+                    newArr[index]=arr1[i];
+                    index++;
+                }
+            }
+
+        }
+        for(int a: newArr)
+        {
+            System.out.print(" "+a);
+
+        }
+
+        //=======================================================================
 
 //        // o WAP to Count the Frequency of Each Element
 //
