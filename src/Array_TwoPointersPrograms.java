189835e1a700748d75b54cb0a07878ba96e581e8
shivamstech <shivamsharma.code@gmail.com>
2026-06-22 20:51:28 +0530

    Array_TwoPointersPrograms_Update17 commit

--- src/Array_TwoPointersPrograms.java
+++ src/Array_TwoPointersPrograms.java
@@ -338,35 +338,37 @@ public class Array_TwoPointersPrograms {
 
     //====================================================================
 
-    // * 167. Two Sum II - Input Array Is Sorted leetcode
+//    // * 167. Two Sum II - Input Array Is Sorted leetcode
+//
+//    public int[] twoSum(int[] numbers, int target)
+//    {
+//        int n = numbers.length;
+//
+//        int L = 0;
+//        int R = n-1;
+//
+//        while(L < R)
+//        {
+//            int sum = numbers[L] + numbers[R];
+//
+//            if(sum == target)
+//            {
+//                return new int[]{L+1, R+1};
+//            }
+//            else if(sum < target)
+//            {
+//                L++;
+//            }
+//            else
+//            {
+//                R--;
+//            }
+//        }
+//
+//        return new int[]{};
+//    }
 
-    public int[] twoSum(int[] numbers, int target)
-    {
-        int n = numbers.length;
-
-        int L = 0;
-        int R = n-1;
-
-        while(L < R)
-        {
-            int sum = numbers[L] + numbers[R];
-
-            if(sum == target)
-            {
-                return new int[]{L+1, R+1};
-            }
-            else if(sum < target)
-            {
-                L++;
-            }
-            else
-            {
-                R--;
-            }
-        }
-
-        return new int[]{};
-    }
+    //===========================================================================
 
 
 }
